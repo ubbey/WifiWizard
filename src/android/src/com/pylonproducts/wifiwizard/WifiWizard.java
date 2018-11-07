80ef58909c9d8d5d34b59a500d6f7b4048c3aad6
vence <vence@VenceZhous-macPro.local>
2018-11-07 15:40:32 +0800

    Change package name

--- src/android/src/com/pylonproducts/wifiwizard/WifiWizard.java
+++ src/android/src/com/pylonproducts/wifiwizard/WifiWizard.java
@@ -12,7 +12,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.pylonproducts.wifiwizard;
+package com.vence.wifiwizard;
 
 import org.apache.cordova.*;
 import java.util.List;
